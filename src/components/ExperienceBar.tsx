836cf4cb5a6dd12601277686ec492c42abc552b8
Gabriel A <tfmdev91@gmail.com>
2021-03-02 07:00:45 -0300

    Day 3-5 done

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from '../styles/components/ExperienceBar.module.css'
+import { ChallengesContext } from './contexts/ChallengesContext';
 
 export function ExperienceBar() {
+
+  const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext)
+
+  const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel
+
   return (
     <header className={styles.experienceBar}>
       <span>8 xp</span>
       <div>
-        <div style={{width: '50%'}}/>
-        <span className={styles.currentExperience} style={{left: '50%'}}>300 xp</span>
+        <div style={{width: `${percentToNextLevel}%`}}/>
+        <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>
+          {currentExperience} xp
+        </span>
       </div>
-      <span>688 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,63 @@
+import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+
 import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
-import {ExperienceBar} from '../components/ExperienceBar'
-import {Profile} from '../components/Profile'
-
-import Head from 'next/head'
+import { ExperienceBar } from '../components/ExperienceBar'
+import { ChallengeBox } from '../components/ChallengeBox'
+import { Profile } from '../components/Profile'
+import { CountdownProvider } from '../components/contexts/CountdownContext'
+import { ChallengesProvider } from '../components/contexts/ChallengesContext'
 
 import styles from '../styles/pages/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | Move.it</title>
-      </Head>
-      <ExperienceBar/>
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-        </div>
-        <div>
-          
-        </div>
-      </section>
-    </div>
+export default function Home(props: HomeProps) {
+
+  console.log(props)
+
+  return (
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience} 
+    challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | Move.it</title>
+        </Head>
+        <ExperienceBar/>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile/>
+              <CompletedChallenges/>
+              <Countdown/>
+            </div>
+            <div>
+              <ChallengeBox/>
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies
+  
+  return {
+    props: {
+      level: Number(level), 
+      currentExperience: Number(currentExperience), 
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
